c4af49ce121d44e53e736799dc1987f88fd94253
Pulkit Sharma <avastpulkit@gmail.com>
2016-04-28 02:20:06 +0530

    Some minor exception handling

--- download.rb
+++ download.rb
@@ -2,11 +2,17 @@ require 'net/http'
 
 model_id = 'd8608cb2'
 
-file_location = Net::HTTP.get_response(URI.parse("http://www.archive3d.net/?a=download&do=get&id="+model_id))['location']
-file_contents = Net::HTTP.get_response(URI.parse(file_location))
+begin
+	
+	file_location = Net::HTTP.get_response(URI.parse("http://www.archive3d.net/?a=download&do=get&id="+model_id))['location']
+	file_contents = Net::HTTP.get_response(URI.parse(file_location))
 
-open(model_id+".zip", "wb") do |file|
-        file.write(file_contents.body)
+	open(model_id+".zip", "wb") do |file|
+	        file.write(file_contents.body)
+	end
+
+rescue
+	puts "ERROR"
+else
+	puts model_id+" downloaded !"
 end
-
-puts "Done."
