24a4f990fa4bbd298635f522ee2e7a6247291753
Pulkit Sharma <avastpulkit@gmail.com>
2016-04-28 02:12:00 +0530

    Initial commit

---
+++ download.rb
@@ -0,0 +1,11 @@
+require 'net/http'
+
+model_id = 'd8608cb2'
+file_location = Net::HTTP.get_response(URI.parse("http://www.archive3d.net/?a=download&do=get&id="+model_id))['location']
+file_contents = Net::HTTP.get_response(URI.parse(file_location))
+
+open("sample.zip", "wb") do |file|
+        file.write(file_contents.body)
+end
+
+puts "Done."
