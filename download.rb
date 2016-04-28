ed95198cc6c1ce3ffe7b86007b6fd877f4bf5bb3
Pulkit Sharma <avastpulkit@gmail.com>
2016-04-29 02:11:58 +0530

    Added search functionality

--- download.rb
+++ download.rb
@@ -1,18 +1,54 @@
 require 'net/http'
+require 'nokogiri'
 
-model_id = 'd8608cb2'
+def download_model(model_id)
 
-begin
-	
-	file_location = Net::HTTP.get_response(URI.parse("http://www.archive3d.net/?a=download&do=get&id="+model_id))['location']
-	file_contents = Net::HTTP.get_response(URI.parse(file_location))
+	puts "Please wait downloading Model"
 
-	open(model_id+".zip", "wb") do |file|
-	        file.write(file_contents.body)
+	begin
+		
+		file_location = Net::HTTP.get_response(URI.parse("http://www.archive3d.net/?a=download&do=get&id="+model_id))['location']
+		file_contents = Net::HTTP.get_response(URI.parse(file_location))
+
+		open(model_id+".zip", "wb") do |file|
+		        file.write(file_contents.body)
+		end
+
+	rescue
+		puts "ERROR downloading"
+	else
+		puts model_id+" downloaded !"
 	end
 
-rescue
-	puts "ERROR"
-else
-	puts model_id+" downloaded !"
 end
+
+
+def search(name,count=1)
+	
+	id_list = []
+
+	begin
+		
+		page = Nokogiri::HTML(Net::HTTP.get_response(URI.parse("http://www.archive3d.net/?tag="+name)).body)
+	
+	rescue
+		puts "There was problem contacting archive3d"
+	else
+		count.times do |x|
+			x = x+1
+			begin
+				id = page.css("#prevtable > div:nth-child(#{x}) > div > a")[0]["href"].split("id=")[1]
+			rescue
+				
+			else
+				id_list << id
+			end
+		end
+	end
+
+	id_list
+end
+
+
+
+p search "vase",2
