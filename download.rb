4b878a4f2d60166eae3174643f335fe111f584e6
Pulkit Sharma <avastpulkit@gmail.com>
2016-04-30 23:20:40 +0530

    Add: format specifier in options

--- download.rb
+++ download.rb
@@ -4,7 +4,19 @@ require 'zip'
 require 'optparse'
 
 
-options = {:input => nil,:count => 1}
+options = {:input => nil,:count => 1,:format => "*"}
+
+
+
+# Dont know how to do case-insensitive glob matching
+formats_glob = {}
+
+formats_glob["3ds"] = "*.3[Dd][Ss]"
+formats_glob["max"] = "*.[Mm][Aa][Xx]"
+formats_glob["gsm"] = "*.[Gg][Ss][Mm]"
+formats_glob["mtl"] = "*.[Mm][Tt][Ll]"
+formats_glob["*"] = "*"
+
 
 parser = OptionParser.new do|opts|
 
@@ -18,6 +30,10 @@ parser = OptionParser.new do|opts|
     options[:count] = count;
   end
 
+  opts.on('-f', '--format FORMAT', "The file format you want. Supported formats are [#{formats_glob.keys.join(",")}]. \n If this flag is not provided then all the available formats are presented to the user") do |format|
+    options[:format] = formats_glob[format];
+  end
+
   opts.on('-h', '--help', 'Displays Help') do
     puts opts
     exit
@@ -27,6 +43,7 @@ end
 parser.parse!
 
 raise "[ERROR] Please provide the INPUT using -i flag" if options[:input].nil?
+raise "[ERROR] Please provide a valid format for the -f flag. Supported formats are [#{formats_glob.keys.join(",")}]" if options[:format].nil?
 
 
 # Downloads a file at a given url and writes it to disk.
@@ -85,23 +102,21 @@ end
 
 # The zip file of the downloaded model contains different 3D formats
 # This method extracts a file with the given format from the zip file.
-# More specifically, it gets the first file which matches the format (glob pattern) and extracts it
-# and saves it with the name output_file and correct format
+# More specifically, it gets the files which matches the format (glob pattern) and extracts them
+# and saves it with the name output_file and their respective format
 #
 # @param input_zip [String] the name of the zip file
 # @param output_file [String] the desired name of the model file.
 # @param format [String] glob pattern for any of the 3D file formats ( for example <tt>".3[Dd][Ss]"</tt>)
 # @return [void]
-def get_model_from_zip(input_zip,output_file,format)
+def get_model_from_zip(input_zip,output_file,format="*")
 
   begin
     Zip::File.open(input_zip) do |zip_file|
-      entry = zip_file.glob(format).first
 
-      begin
-        entry.extract(output_file+"."+entry.name.split('.')[1])
-      rescue Zip::DestinationFileExistsError
-
+      matches = zip_file.glob(format)
+      matches.each do |entry|
+        entry.extract(output_file+"."+entry.name.split('.')[1..-1].join('.')) rescue Zip::DestinationFileExistsError
       end
 
     end
@@ -140,6 +155,7 @@ def search_models(name,count=1)
     end
   end
 
+  puts "Found #{id_list.size} models ! \n"
   id_list
 end
 
@@ -159,4 +175,6 @@ def get_models(name,count=1,format="*.3[Dd][Ss]")
 end
 
 
-get_models(options[:input],options[:count])
+
+get_models(options[:input],options[:count],options[:format])
+#get_model_from_zip("6384f7c8.zip","file","*.[Gg][Ss][Mm]")
